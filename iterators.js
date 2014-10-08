82d80e9740df39a90288478c0425118b6dd4aa29
= <lasazawa@gmail.com>
2014-10-08 10:15:15 -0700

    has map and each test cases

--- iterators.js
+++ iterators.js
@@ -7,7 +7,40 @@ var iterators = {
         }
       }
       return max;
+    },
+
+    min: function(numList) {
+      var min = Infinity;
+      for (var i = numList.length - 1; i >= 0; i--) {
+        if (numList[i] < min) {
+          min = numList[i];
+        }
+      }
+      return min;
+    },
+
+    each: function(arr, callback) {
+      //if arr is null, use an empty array
+      arr = arr || [];
+      for (var i = arr.length - 1; i >= 0; i--) {
+        callback(arr[i]);
+      }
+    },
+
+    map: function (arr) {
+      arr = arr || [];
+      for (var i = arr.length - 1; i >= 0; i--) {
+        return arr[i];
+      }
     }
-  };
+
+
+
+};
 
 module.exports = iterators;
+
+
+var cased = names.map(function (person) {
+    return person.toUpperCase();
+});
--- spec/iterators_test.js
+++ spec/iterators_test.js
@@ -2,7 +2,9 @@ var expect = require('chai').expect;
 var Iterators = require('../iterators.js');
 
 describe('Iterators', function () {
-  describe('#max', function () {
+
+
+  describe('max', function () {
 
     // cleaner to declare the variable outside of the beforeEach function
     var myArr;
@@ -10,7 +12,7 @@ describe('Iterators', function () {
     // use beforeEach(function(){}); to create a variable before each test is run.
     // This prevents overwriting when we have multiple tests and manipulate multiple values
     beforeEach(function () {
-      myArr = [66,22,67, 34];
+      myArr = [66, 22, 67, 34];
     });
 
     it('should return the maximum in an array', function () {
@@ -21,4 +23,42 @@ describe('Iterators', function () {
       expect(Iterators.max(myArr)).to.equal(-Infinity);
     });
   });
+
+  describe("min", function() {
+      var myArr;
+      beforeEach(function () {
+        myArr = [66, 22, 67, 34];
+      });
+
+      it("should return the min number of myArr", function() {
+          expect(Iterators.min(myArr)).to.equal(22);
+      });
+      it("should return Infinity for empty array", function(){
+          myArr = [];
+          expect(Iterators.min(myArr)).to.equal(Infinity);
+      });
+  });
+
+
+  describe("each", function() {
+      var myArr = [2, 5, 10, 200];
+      it("should return each number in the array", function() {
+          var local = [];
+          Iterators.each([1, 2], function(num){
+            local.push(num);
+          });
+          expect(local).to.include.members([1, 2]);
+      });
+  });
+
+    describe("Map", function() {
+      var myArr = [2, 3, 4, 5];
+      it("should return an new array where every number is doubled", function() {
+          var double = function(a) {
+            return a * 2;
+          };
+          expect(Iterators.map(myArr, double)).to.include.members([4, 6, 8, 10]);
+      });
+  });
+
 });
